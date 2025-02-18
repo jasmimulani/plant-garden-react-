a0791e06503e3e695b32b5fcdc55bcb18d1ed407
Jasmin <jasmimulani@gmail.com>
2025-02-18 19:18:03 +0530

    "18/02/24"

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,3 +1,24 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import { Auth0Provider } from '@auth0/auth0-react';
+// import App from './App';
+
+// const root = createRoot(document.getElementById('root'));
+
+// root.render(
+// <Auth0Provider
+//     domain="dev-1s4phe7nqz0tvedv.us.auth0.com"
+//     clientId="jd37E7BxzJI7oUDz50Qw4MYlDx7btT9m"
+//     authorizationParams={{
+//       redirect_uri: window.location.origin
+//     }}
+//   >
+//     <App />
+//   </Auth0Provider>,
+// );
+
+
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
